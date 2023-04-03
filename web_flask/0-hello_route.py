5906914c8a74471c03f9219181cd850fbd31e9f3
CllaudiaB <buraanna27@gmail.com>
2023-04-03 06:13:43 -0700

    added documentation file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,18 @@
 Script that starts a Flask web application
 """
 
+
 from flask import Flask
 
-capp = Flask(__name__)
+
+app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_world():
     """fonction that display Hello HBNB!"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')

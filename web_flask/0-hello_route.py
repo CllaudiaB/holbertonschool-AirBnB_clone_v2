bfdb6148ecf99f6769bae8cf02e61fc4fc9b639c
CllaudiaB <buraanna27@gmail.com>
2023-04-03 05:59:58 -0700

    documented function hello_world

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,9 +9,8 @@ capp = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-    #display Hello HBNB!
+    """display Hello HBNB!"""
     return "<p>Hello HBNB!</p>"
 
 if __name__ == '__main__':
-    # run app in debug mode on port 5000
     app.run(debug=True, port=5000, host='0.0.0.0')

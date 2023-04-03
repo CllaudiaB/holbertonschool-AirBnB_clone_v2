79bc409bd7f2e249ce903375eebac80f85058ed8
CllaudiaB <buraanna27@gmail.com>
2023-04-03 10:24:39 -0700

    adding file for task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    """function that display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    """function that display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """function that display “C ” followed by the value of the text variable
+       (replace underscore _ symbols with a space )
+    """
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def display_py_text(text='is cool'):
+    """function that display “Python ”, followed by the value of the text
+       variable (replace underscore _ symbols with a space )
+    """
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_number(n):
+    """display “n is a number” only if n is an integer"""
+    if type(n) is int:
+        return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')

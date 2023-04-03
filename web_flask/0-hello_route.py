6c6771672423f285b14bc275d567f4d846c047d2
CllaudiaB <buraanna27@gmail.com>
2023-04-03 05:54:46 -0700

    adding file for task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask
+
+capp = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "<p>Hello HBNB!</p>"
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host='0.0.0.0')

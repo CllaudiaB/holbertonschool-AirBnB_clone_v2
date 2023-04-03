7c6afcdbd64f449dc20bfdbb3daca9ad1fcf1db3
CllaudiaB <buraanna27@gmail.com>
2023-04-03 12:02:52 -0700

    adding file for task 5, delete if condition, addind templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,7 @@ Script that starts a Flask web application
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -46,6 +46,11 @@ def display_number(n):
     """display “n is a number” only if n is an integer"""
     return f'{n} is a number'
 
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_html(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
